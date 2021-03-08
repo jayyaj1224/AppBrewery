34c80e51f6795dc72f7c86a4cda1f89b9c9931ab
Jay Lee <jayyaj1224@gmail.com>
2021-03-08 17:22:50 +0900

    ㄴㅇㄹ

--- Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["Four + sdfsfsis sdfa","True"],
+        ["Foursdfs sdfa","True"],
+        ["Fousadfsdfa","False"],
+        ["Four + Two is sdfa","True"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
-
-
+    
+    @IBAction func answerButtonClicked(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+            print(questionNumber)
+        } else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 }
+
+
 
