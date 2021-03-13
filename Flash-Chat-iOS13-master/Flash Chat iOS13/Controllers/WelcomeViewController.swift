f7a31773cac909fd3a2477b94b32217f1b8ccbdc
Jay Lee <jayyaj1224@gmail.com>
2021-03-13 14:48:15 +0900

    animation
    animation

--- Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat-iOS13-master/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -9,14 +9,41 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel2: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        titleLabel2.alpha = 0
+        titleLabel.text = "Welcome to"
+        titleLabel2.text = "🔥FireChat"
+        
+        for n in 1...10 {
+            Timer.scheduledTimer(withTimeInterval: 0.07*Double(n), repeats: false) { (timer) in
+                self.titleLabel.alpha = CGFloat(n)*0.1
+            }
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: 1.4, repeats: false) { (timer) in
+            for n in 0...10 {
+                Timer.scheduledTimer(withTimeInterval: 0.08*Double(n), repeats: false) { (timer) in
+                    self.titleLabel.alpha = (10.0-CGFloat(n))*0.1
+                }
+            }
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: 2.3, repeats: false) { (timer) in
+            for n in 0...10 {
+                Timer.scheduledTimer(withTimeInterval: 0.08*Double(n), repeats: false) { (timer) in
+                    self.titleLabel2.alpha = CGFloat(n)*0.1
+                }
+            }
+        }
+        
     }
     
-
 }
+
+
