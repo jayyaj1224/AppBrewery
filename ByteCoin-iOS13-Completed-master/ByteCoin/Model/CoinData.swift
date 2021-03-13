51bd0256e4b303610c1c42870b70b9540e46efdd
Jay Lee <jayyaj1224@gmail.com>
2021-03-13 22:46:55 +0900

    ㅇㄴㄹ

---
+++ ByteCoin-iOS13-Completed-master/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Angela Yu on 11/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
